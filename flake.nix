89af20541eccff5e2c9d3d4bd14d582dcb4edf33
lionhairdino <lionhairdino@gmail.com>
2025-04-18 11:14:56 +0900

    .gitignore

--- flake.nix
+++ flake.nix
@@ -1,14 +1,18 @@
 {
   description = "Hello World package";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.hello = with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      packages.${system}.hello = pkgs.stdenv.mkDerivation {
         pname = "hello";
         version = "1.0";
         src = ./.;
         buildPhase = "echo 'Hello from external flake!' > hello.txt";
         installPhase = "mkdir -p $out; cp hello.txt $out/";
       };
+    defaultPackage.${system} = self.packages.${system}.hello;
   };
 }
